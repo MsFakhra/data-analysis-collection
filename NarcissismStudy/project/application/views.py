fbc41dff446041e78e638d59c5e36a4fce3f1361
Usman <usman@boldking.com>
2020-06-13 00:18:44 +0200

    updated gitignore

--- NarcissismStudy/project/application/views.py
+++ NarcissismStudy/project/application/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("request, 'hello_world.html', {}")
+    return HttpResponse("request, 'hello_world.html hehe', {}")
