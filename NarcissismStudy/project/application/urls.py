943256f9b20fa8c36a0b791082b768820f260f78
Fakhra Jabeen <fakhraikram@yahoo.com>
2020-06-12 15:27:06 -0700

    changes

--- NarcissismStudy/project/application/urls.py
+++ NarcissismStudy/project/application/urls.py
@@ -3,4 +3,5 @@ from application import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('inputdata', views.inputdata, name='inputdata'),
 ]
--- NarcissismStudy/project/application/views.py
+++ NarcissismStudy/project/application/views.py
@@ -1,5 +1,18 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
+<<<<<<< Updated upstream
     return HttpResponse("request, 'hello_world.html hehe', {}")
+=======
+    return HttpResponse("request, 'hello_world.html', {}")
+
+def inputdata(request):
+    # This just create the folder where I want to save the image.
+    profile = 'usmanmaliktest'
+    if not os.path.exists('/TrainingDataset/' + profile):
+        os.mkdir('/TrainingDataset/' + profile)
+    return HttpResponse("Processed")
+>>>>>>> Stashed changes
