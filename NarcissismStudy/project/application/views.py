78e062bb13a2ddef18743f3f2c50845660164c08
Usman <usman@boldking.com>
2020-06-14 23:40:49 +0200

    saving form data

--- NarcissismStudy/project/application/views.py
+++ NarcissismStudy/project/application/views.py
@@ -1,12 +1,22 @@
 import pathlib
 import os
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from application.models import Users
 
 def index(request):
     return render(request, 'index.html', {})
 
 def step2(request):
+    user = Users(
+        full_name = request.POST['full_name'],
+        email = request.POST['email'],
+        instagram = request.POST['instagram'],
+        created_at = datetime.datetime.now(),
+    )
+    user.save()
+    print (request.FILES)
     return render(request, 'step2.html', {})
 
 def thankyou(request):
