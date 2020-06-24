06b8754d986862e7957318a54a0492f74e510b50
msikram <fakhraikram@yahoo.com>
2020-06-24 22:12:17 +0200

    Database created

--- NarcissismStudy/project/application/models.py
+++ NarcissismStudy/project/application/models.py
@@ -12,9 +12,35 @@ class Users(models.Model):
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
     created_at = models.DateTimeField('created')
+    state = models.TextField('state', default= 'pending')
 
 class Posts(models.Model):
-    postid = models.BigIntegerField(primary_key=True)
-    instagram = models.CharField(max_length=200)
-    url = models.TextField()
-    hashtags = models.TextField()
+    user_id = models.BigIntegerField('user_id') #P.K
+    instagram = models.CharField('instagram',max_length=200)  #F.K
+    posted_on = models.DateTimeField('posted_on')
+    post_url = models.TextField('post_url')
+    hashtags = models.TextField('hashtags')
+    mentions = models.TextField('mentions')
+    tagged_users = models.TextField('tagged_users')
+    is_video = models.BooleanField('is_video', default= False)
+    likes = models.BigIntegerField('likes')
+    caption = models.TextField('caption')
+
+class Comment(models.Model):
+    post_id = models.BigIntegerField('post_id')  # F.K
+    posted_on = models.DateTimeField('posted_on')
+    owner = models.CharField(max_length=200)
+    is_comment = models.BooleanField('is_comment') #False if its a reply
+    sentiment = models.CharField('sentiment', max_length=200)
+    sscore = models.FloatField('sscore')
+    tag_used = models.BooleanField ('tag_used', default= False) #True if the tag is used
+    likes = models.IntegerField('likes')
+    caption = models.TextField('caption')
+
+class Picture(models.Model):
+    post_id = models.BigIntegerField('post_id')  # F.K
+    posted_on = models.DateTimeField('posted_on')
+    selfie = models.BooleanField('selfie', default= False)  # True if the tag is used
+    person = models.TextField('person') #include which person was identified
+    image_path = models.TextField('image_path')
+
