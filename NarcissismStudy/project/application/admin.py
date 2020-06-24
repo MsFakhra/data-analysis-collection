0a6cfad4d416b0f18f544fde91d701a6396a5bde
Usman <usman@boldking.com>
2020-06-24 22:17:36 +0200

    updated admin

--- NarcissismStudy/project/application/admin.py
+++ NarcissismStudy/project/application/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Users
+from .models import Posts
+from .models import Comment
+from .models import Picture
 
 # Register your models here.
 admin.site.register(Users)
+admin.site.register(Posts)
+admin.site.register(Comment)
+admin.site.register(Picture)
