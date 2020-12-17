3ddc66bd01d6318df86e946d5219f628392e5645
usman ahmed malik <usman@boldking.com>
2020-12-17 22:08:07 +0100

    updating test

--- NarcissismStudy/test.py
+++ NarcissismStudy/test.py
@@ -1,11 +1,21 @@
 import sqlite3
 
+conn = sqlite3.connect('project/db.sqlite3')
+
+
+def extract_information(id):
+    print("Extracting Information")
+    cursor = conn.execute("SELECT instagram from application_users WHERE id =" + str(id) + ";")
+    for row in cursor:
+        profilename = row[0]
+    print(profilename)
+
 def startjob():
     print("Job started")
-    conn = sqlite3.connect('project/db.sqlite3')
     cursor = conn.execute("SELECT * from application_users WHERE state = 'pending' AND invalid = 0;")
     for row in cursor:
-        print("record = ", row)
+        id = row[0]
+        extract_information(id)
 
 if __name__ == '__main__':
     print("main")
